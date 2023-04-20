6c68a1220ff206ea468a40eb88b64ce4634d68ec
Rozelin <rozelin.dc@gmail.com>
2023-04-20 16:14:38 +0900

    曲選択後にゲームに進めるように

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import React, { useEffect, useState, useMemo, useRef } from 'react'
+import React, { useEffect, useState, useMemo, useCallback } from 'react'
 import { IPlayerApp, Player, PlayerListener } from 'textalive-app-api'
-import { MdPlayCircleOutline } from 'react-icons/md'
-import './style.css'
+import { SongName, songData } from './utils/songData'
+import SongSelect from './SongSelect'
+import Game from './Game'
+import './app.css'
 
 export default function App() {
   const [player, setPlayer] = useState<Player>()
   const [app, setApp] = useState<IPlayerApp>()
-  const [isPlayed, setIsPlayed] = useState(false)
-  const [isReady, setIsReady] = useState(false)
+  const [songName, setSongName] = useState<SongName | ''>('')
+  const [isVideoReady, setIsVideoReady] = useState(false)
+  const [isTimerReady, setIsTimerReady] = useState(false)
   const [position, setPosition] = useState(-1)
   const [mediaElement, setMediaElement] = useState<HTMLDivElement | null>(null)
 
@@ -31,28 +34,17 @@ export default function App() {
 
     const playerListener: PlayerListener = {
       onAppReady: (app) => {
-        if (!app.songUrl) {
-          p.createFromSongUrl('https://piapro.jp/t/Vfrl/20230120182855', {
-            video: {
-              // 音楽地図訂正履歴: https://songle.jp/songs/2427950/history
-              beatId: 4267334,
-              chordId: 2405059,
-              repetitiveSegmentId: 2475645,
-              // 歌詞タイミング訂正履歴: https://textalive.jp/lyrics/piapro.jp%2Ft%2FVfrl%2F20230120182855
-              lyricId: 56095,
-              lyricDiffId: 9637,
-            },
-          })
-        }
         setApp(app)
       },
+      onVideoReady: () => {
+        setIsVideoReady(true)
+      },
       onTimerReady: () => {
-        setIsReady(true)
+        setIsTimerReady(true)
       },
       onTimeUpdate: (position) => {
         setPosition(position)
       },
-      onPlay: () => setIsPlayed(true),
     }
     p.addListener(playerListener)
 
@@ -64,16 +56,56 @@ export default function App() {
     }
   }, [mediaElement])
 
+  const handleSongSelect = useCallback(
+    (songName: SongName) => {
+      if (!player) {
+        return
+      }
+
+      player.createFromSongUrl(
+        songData[songName].songUrl,
+        songData[songName].songUrlOptions
+      )
+      setSongName(songName)
+    },
+    [player]
+  )
+
+  const handlePlay = useCallback(() => {
+    if (!player) {
+      return
+    }
+
+    player.requestPlay()
+  }, [player])
+
+  const handleStop = useCallback(() => {
+    if (!player) {
+      return
+    }
+
+    player.requestStop()
+    setIsVideoReady(false)
+    setIsTimerReady(false)
+    setSongName('')
+    setPosition(-1)
+  }, [player])
+
   return (
     <div className='app'>
-      {!isPlayed && player && (
-        <button onClick={() => player.requestPlay()} className='play-button'>
-          <MdPlayCircleOutline />
-        </button>
+      {songName === '' ? (
+        <SongSelect onSelect={handleSongSelect} />
+      ) : (
+        <Game
+          songName={songName}
+          player={player}
+          position={position}
+          onPlay={handlePlay}
+          onStop={handleStop}
+          isVideoReady={isVideoReady}
+          isTimerReady={isTimerReady}
+        />
       )}
-      <div className='wrapper'>
-        <div className='char'>{char}</div>
-      </div>
       {media}
     </div>
   )
