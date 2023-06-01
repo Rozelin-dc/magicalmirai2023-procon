63de783d95d417d874b2906aaad687ee2ad77f40
Rozelin <rozelin.dc@gmail.com>
2023-06-01 16:46:07 +0900

    vitest導入

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
@@ -17,5 +18,8 @@ export default defineConfig({
         additionalData: `@import "./src/_variables.scss";`
       }
     }
+  },
+  test: {
+    include: ['src/**/*.test.ts'],
   }
 })
