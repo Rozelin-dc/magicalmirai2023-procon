d823f9ab1139622419c8530c68563a6c1cf92e1c
Rozelin <rozelin.dc@gmail.com>
2025-06-29 18:06:43 +0900

    修正

--- scripts/diff-match/index.sh
+++ scripts/diff-match/index.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-tool_root_dir="./scripts"
-
-project_root_dir=".."
+project_root_dir=$(pwd)
 # Directory where you want to store the mapping data (relative path from project_root_dir).
-map_file_dir="$project_root_dir/id-map-data"
-
-
-cd $project_root_dir
+map_file_dir="id-map-data"
 
 # Receives a relative path of a file name as the first argument and performs a structural match as needed.
 diff_match() {
@@ -26,9 +21,7 @@ diff_match() {
     git config --local difftool.gumtree-docker.cmd "docker run -v \$REMOTE:/diff/left -v \$LOCAL:/diff/right -p 4567:4567 rozelin/gumtree:latest axmldiff left/$file_path right/$file_path"
     git difftool -d --no-symlinks -t gumtree-docker @{u} > $map_file_path.diff.xml
 
-    cd $tool_root_dir
-    node ./diff-match/main.mjs --file $map_file_path
-    cd $project_root_dir
+    node ./scripts/diff-match/main.mjs --file $map_file_path
   fi
 }
 
@@ -36,7 +29,7 @@ set -e
 
 base_branch="$1"
 head_branch="$2"
-target_dir="ui/"
+target_dir="src/"
 
 git fetch origin "$base_branch":"origin/$base_branch"
 git fetch origin "$head_branch":"origin/$head_branch"
--- scripts/id-embedding/index.sh
+++ scripts/id-embedding/index.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-tool_root_dir="./scripts"
-
-project_root_dir=".."
+project_root_dir=$(pwd)
 # Directory where you want to store the mapping data (relative path from project_root_dir).
 map_file_dir="$project_root_dir/id-map-data"
 
-cd $tool_root_dir
-
-
-files=$(find $project_root_dir/ui)
+files=$(find $project_root_dir/src)
 target_file_pattern="^.+\.((t|j)sx|svg|component\.js)$"
 
 max_jobs=4
@@ -22,7 +17,7 @@ for file in $files; do
 
   if [[ $file =~ $target_file_pattern ]]; then
     # If $file is TSX, JSX, or SVG file, do ID embedding.
-    node ./id-embedding/main.mjs --file $file --projectRootDir $project_root_dir --mapFileDir $map_file_dir &
+    node ./scripts/id-embedding/main.mjs --file $file --projectRootDir $project_root_dir --mapFileDir $map_file_dir &
     ((job_count++))
 
     if ((job_count >= max_jobs)); then
