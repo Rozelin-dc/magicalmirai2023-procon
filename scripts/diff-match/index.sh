368deb38852eabe9389ee8b07829e3ac0d5d3bd8
Rozelin <rozelin.dc@gmail.com>
2025-06-29 18:09:16 +0900

    スラッシュ抜け

--- scripts/diff-match/index.sh
+++ scripts/diff-match/index.sh
@@ -15,7 +15,7 @@ diff_match() {
 
   if [[ $file =~ $target_file_pattern ]]; then
     file_path=${file#./}
-    map_file_path=${project_root_dir%/}${map_file_dir%/}/$file_path
+    map_file_path=${project_root_dir%/}/${map_file_dir%/}/$file_path
 
     # If $file is TSX, JSX, or SVG file, do diff match.
     git config --local difftool.gumtree-docker.cmd "docker run -v \$REMOTE:/diff/left -v \$LOCAL:/diff/right -p 4567:4567 rozelin/gumtree:latest axmldiff left/$file_path right/$file_path"
