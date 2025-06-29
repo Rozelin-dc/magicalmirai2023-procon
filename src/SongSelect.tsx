ba236635bd129ad37e5f2ac02b487d17782ddc3f
Rozelin <rozelin.dc@gmail.com>
2025-06-29 18:07:49 +0900

    適当な変更

--- src/SongSelect.tsx
+++ src/SongSelect.tsx
@@ -11,7 +11,7 @@ export default function SongSelect({
   return (
     <div className='song-select-container'>
       <div className='caption-container'>
-        <div className='caption'>
+        <div className='caption-ababa'>
           {
             '※ローマ字表記の方式は右側の設定ボタンから変更できます(デフォルトはヘボン式です)。ただし、どの方式の場合でも長音の処理はしていません。また、読みと書きが異なる文字は、書き準拠の表記です。例えば、「歌を歌おう」は「UTAWO UTAOU」になります。'
           }
