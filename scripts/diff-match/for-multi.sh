d52f7925ed653cfd333e0e3392fa3e0e59abf87c
Rozelin <rozelin.dc@gmail.com>
2025-07-12 11:57:58 +0900

    dockerの終了コードも確認

--- scripts/diff-match/for-multi.sh
+++ scripts/diff-match/for-multi.sh
@@ -92,6 +92,10 @@ for group_files in "${actual_file_groups[@]}"; do
 
   # Do diff matching
   docker run --rm -v "$after_tmp:/diff/left" -v "$before_tmp:/diff/right" -p 4567:4567 rozelin/gumtree:latest axmldiff left/$tmp_id.tsx right/$tmp_id.tsx > "$map_file_tmp_dir/$tmp_id.diff.xml"
+
+  docker_exit_code=$?
+  echo "docker exited with code $docker_exit_code"
+
   node ./scripts/diff-match/for-multi.mjs --file $map_file_tmp_dir/$tmp_id.diff.xml --multiFiles $actual_files --projectRootDir $project_root_dir --idMapDir $map_file_dir --beforeTmpDir $before_tmp --afterTmpDir $after_tmp
 
   node_exit_code=$?
