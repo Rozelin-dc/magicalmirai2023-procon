56eed934b6ba19228ad8e6d5e2574701e9a39b34
Rozelin <rozelin.dc@gmail.com>
2025-07-12 12:48:10 +0900

    driverをテストごとに独立して作るように

--- e2e/top.test.js
+++ e2e/top.test.js
@@ -7,44 +7,46 @@ import selenium from 'selenium-webdriver'
 import chrome from 'selenium-webdriver/chrome.js'
 import { injectDriver } from '@rozelin-dc/js-uitestfix'
 
+const createDriver = async (testName) => {
+  const options = new chrome.Options()
+  options.addArguments('--headless', '--window-size=1920,1080')
+  let driver = await new selenium.Builder()
+    .forBrowser('chrome')
+    .setChromeOptions(options)
+    .build()
+  const __dirname = path.dirname(fileURLToPath(import.meta.url))
+  driver = await injectDriver(driver, testName, {
+    outputDir: path.join(__dirname, '../output'),
+    answerDataDir: path.join(__dirname, '../answer-data'),
+    configPath: path.join(__dirname, '../config.properties'),
+    resultSavePath: path.join(__dirname, '../result'),
+  })
+  await driver.get('http://localhost:1123')
+  return driver
+}
+
 describe('test typing-lyrics top page', () => {
-  /** @type {selenium.WebDriver} */
-  let driver
+  it('get caption', async (context) => {
+    const driver = await createDriver(context.name)
 
-  beforeEach(async (context) => {
-    const options = new chrome.Options()
-    options.addArguments('--headless', '--window-size=1920,1080')
-    driver = await new selenium.Builder()
-      .forBrowser('chrome')
-      .setChromeOptions(options)
-      .build()
-    const __dirname = path.dirname(fileURLToPath(import.meta.url))
-    driver = await injectDriver(driver, context.name, {
-      outputDir: path.join(__dirname, '../output'),
-      answerDataDir: path.join(__dirname, '../answer-data'),
-      configPath: path.join(__dirname, '../config.properties'),
-      resultSavePath: path.join(__dirname, '../result'),
-    })
-    await driver.get('http://localhost:1123')
-  })
-
-  afterEach(async () => {
-    await driver.quit()
-  })
-
-  it('get caption', async () => {
     const captionElement = await driver.findElement(
       selenium.By.className('caption')
     )
     const captionText = await captionElement.getText()
     assert.equal(captionText.includes('UTAWO UTAOU'), true)
+
+    await driver.quit()
   })
 
-  it('get caption container', async () => {
+  it('get caption container', async (context) => {
+    const driver = await createDriver(context.name)
+
     const captionElement = await driver.findElement(
       selenium.By.className('caption-container')
     )
     const visible = await captionElement.isDisplayed()
     assert.equal(visible, true)
+
+    await driver.quit()
   })
 })
